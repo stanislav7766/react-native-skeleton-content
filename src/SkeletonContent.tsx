bd307c3ef3d26bdcdfb0929edb9a2fd122e81a5e
Stanislav Tikhonov <macbook@MacBook-Air-Macbook.local>
2022-12-08 15:07:02 +0200

    temp

--- src/SkeletonContent.tsx
+++ src/SkeletonContent.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react/no-array-index-key */
 import * as React from 'react';
 import { LayoutChangeEvent, StyleSheet, View } from 'react-native';
@@ -17,8 +18,8 @@ import {
   DEFAULT_LOADING,
   ISkeletonContentProps,
 } from './Constants';
-import StaticBone from './StaticBone';
-import ShiverBone from './ShiverBone';
+// import StaticBone from './StaticBone';
+// import ShiverBone from './ShiverBone';
 
 const { useState, useCallback } = React;
 
@@ -79,94 +80,18 @@ function SkeletonContent({
     return x;
   });
 
-  const getBoneContainer = (
-    layoutStyle: ICustomViewStyle,
-    childrenBones: JSX.Element[],
-    key: number | string
-  ) => (
-    <View key={layoutStyle.key || key} style={layoutStyle}>
-      {childrenBones}
-    </View>
-  );
-
   const getBones = (
     bonesLayout: ICustomViewStyle[] | undefined,
-    childrenItems: any,
-    prefix: string | number = ''
+    childrenItems: any
   ): JSX.Element[] => {
     if (bonesLayout && bonesLayout.length > 0) {
-      const iterator: number[] = new Array(bonesLayout.length).fill(0);
-      return iterator.map((_, i) => {
-        // has a nested layout
-        if (bonesLayout[i].children && bonesLayout[i].children!.length > 0) {
-          const containerPrefix = bonesLayout[i].key || `bone_container_${i}`;
-          const { children: childBones, ...layoutStyle } = bonesLayout[i];
-          return getBoneContainer(
-            layoutStyle,
-            getBones(childBones, [], containerPrefix),
-            containerPrefix
-          );
-        }
-        if (animationType === 'pulse' || animationType === 'none') {
-          return (
-            <StaticBone
-              backgroundPulseColor={backgroundPulseColor}
-              boneLayout={bonesLayout[i]}
-              key={prefix ? `${prefix}_${i}` : i}
-              componentSizeWidth={componentSize.width as number}
-              componentSizeHeight={componentSize.height as number}
-              animationType={animationType}
-              animationDirection={animationDirection}
-              boneColor={boneColor}
-            />
-          );
-        }
-        return (
-          <ShiverBone
-            animationValue={animationValue}
-            highlightColor={highlightColor}
-            boneLayout={bonesLayout[i]}
-            key={prefix ? `${prefix}_${i}` : i}
-            componentSizeWidth={componentSize.width as number}
-            componentSizeHeight={componentSize.height as number}
-            animationType={animationType}
-            animationDirection={animationDirection}
-            boneColor={boneColor}
-          />
-        );
-      });
-      // no layout, matching children's layout
+      return [];
     }
-    return React.Children.map(childrenItems, (child, i) => {
-      const styling = child.props.style || {};
-
+    return React.Children.map(childrenItems, () => {
       if (animationType === 'pulse' || animationType === 'none') {
-        return (
-          <StaticBone
-            backgroundPulseColor={backgroundPulseColor}
-            boneLayout={styling}
-            key={i}
-            componentSizeWidth={componentSize.width as number}
-            componentSizeHeight={componentSize.height as number}
-            animationType={animationType}
-            animationDirection={animationDirection}
-            boneColor={boneColor}
-          />
-        );
+        return null;
       }
-      return (
-        <ShiverBone
-          animationValue={animationValue}
-          highlightColor={highlightColor}
-          boneLayout={styling}
-          key={i}
-          componentSizeWidth={componentSize.width as number}
-          componentSizeHeight={componentSize.height as number}
-          animationType={animationType}
-          animationDirection={animationDirection}
-          boneColor={boneColor}
-        />
-      );
+      return null;
     });
   };
 
